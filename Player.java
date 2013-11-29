11711740fb1bc2c85ebddcd620aa31c2e195fcc2
asdf2345 <noremac428@gmail.com>
2013-11-29 14:12:42 -0600

    player throws on update again

--- Player.java
+++ Player.java
@@ -48,7 +48,13 @@ public class Player extends Entity{
 	@Override
 	public void update(GameContainer container, StateBasedGame game, int delta) throws SlickException {
 		super.update(container, game, delta);
-
+		if(itemsPending.size() > 0 && itemThrowDelay++ % 5 == 0){
+			throwItem();
+						
+		}
+		if(itemThrowDelay > 100){
+			itemThrowDelay = 0;
+		}
 	}
 
 
@@ -94,43 +100,6 @@ public class Player extends Entity{
 	public Rectangle getSpace() {
 		return new Rectangle(x - MainGameState.TILE_SIZE, y - MainGameState.TILE_SIZE, width + MainGameState.TILE_SIZE, height + MainGameState.TILE_SIZE);
 	}
-
-//	public static Player restorePlayer(File playerSaveFile) {
-//		Player tempPlayer = new Player();
-//		EntityData tempPlayerData = null;
-//		
-//		Gson gson = new Gson();
-//		
-//		try {
-//			Scanner reader = new Scanner(new BufferedReader(new FileReader(playerSaveFile)));
-//			if(reader.hasNext()) {
-//				tempPlayerData = gson.fromJson(reader.next(),EntityData.class);
-//				tempPlayer.x = tempPlayerData.getX();
-//				tempPlayer.y = tempPlayerData.getY();
-//				tempPlayer.inventory = new Inventory(tempPlayerData.getInventoryIdArray());
-//				tempPlayer.health = tempPlayerData.getHealth();
-//			}
-//			reader.close();
-//		} catch (FileNotFoundException e) {
-//			e.printStackTrace();
-//		}
-//		
-//		return tempPlayer;
-//	}
-	
-//	public void savePlayer() {
-//		try {
-//			BufferedWriter writer = new BufferedWriter(new FileWriter(SlimeGame.basePath + "//res//playersave.json"));
-//			Gson gson = new Gson();
-//			
-//			EntityData playerData = new EntityData(this);
-//			
-//			writer.write(gson.toJson(playerData));
-//			writer.close();
-//		} catch (IOException e) {
-//			e.printStackTrace();
-//		}
-//	}
 
 	public int[] getInventoryIdArray() {
 		return inventory.getIdArray();
